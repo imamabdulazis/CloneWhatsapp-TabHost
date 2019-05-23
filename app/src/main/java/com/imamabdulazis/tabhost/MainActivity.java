f647940c45ac3a2f123f0ce48c7e5f1a62299075
imun <imamabdulazis028@gmail.com>
2019-05-23 14:19:13 +0700

    fix projek

---
+++ app/src/main/java/com/imamabdulazis/tabhost/MainActivity.java
@@ -0,0 +1,37 @@
+package com.imamabdulazis.tabhost;
+
+import android.app.Activity;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.imamabdulazis.tabhost.adapter.ViewPagerAdapter;
+import com.imamabdulazis.tabhost.fragment.ChatFragment;
+import com.imamabdulazis.tabhost.fragment.StatusFragment;
+
+public class MainActivity extends AppCompatActivity {
+    Activity activity=this;
+    private String TAG=getClass().getSimpleName();
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager=findViewById(R.id.viewpager);
+        tabLayout=findViewById(R.id.tblayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this.getSupportFragmentManager());
+
+        adapter.addFrag(new ChatFragment(), "Chat");
+        adapter.addFrag(new StatusFragment(), "Status");
+        adapter.addFrag(new ChatFragment(), "Call");
+        viewPager.setAdapter(adapter);
+    }
+
+
+}
